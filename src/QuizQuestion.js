f57cf26d2bd5155bf0345b47d6c9e154e8477d70
Ajay <ajaymungur@hotmail.com>
2019-04-12 17:41:52 +0100

    Module 6 completed - Adds incorrectAnswer state and error message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,17 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component{
 
+  constructor(props){
+    super(props)
+    this.state = { incorrectAnswer: false }
+  }
+
   handleClick(button_text){
     if (button_text === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false })
       this.props.showNextQuestionHandler()
+    } else {
+      this.setState({ incorrectAnswer: true })
     }
   }
 
@@ -26,6 +34,7 @@ class QuizQuestion extends Component{
             })}
           </ul>
         </section>
+          {this.state.incorrectAnswer ? <p className="error">Sorry, thats not right</p> : null}
       </main>
     )
   }
